dafaf46cc04815555ade0755ccfca7e2bcc32d51
you(r)-ink <60732521+you-ink@users.noreply.github.com>
2022-10-31 12:24:44 +0700

    Setting Config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+// $config['base_url'] = '';
+$http = (isset($_SERVER['HTTPS'])) ? "https://" : "http://";
+$url = str_replace("index.php", "", $_SERVER['SCRIPT_NAME']);
+$config['base_url'] = $http.$_SERVER['SERVER_NAME'].$url;
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +38,7 @@ $config['base_url'] = '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------

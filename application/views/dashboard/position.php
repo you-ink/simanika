609716d2a297f5d2165374207e76c3721d9313a0
devyaprilliah <117129252+devyaprilliah@users.noreply.github.com>
2022-11-03 15:55:58 +0700

    add position

--- application/views/dashboard/position.php
+++ application/views/dashboard/position.php
@@ -15,69 +15,37 @@
                     <table class="table mb-0">
                       <thead class="bg-light">
                         <tr>
-                          <th scope="col" class="border-0">#</th>
-                          <th scope="col" class="border-0">Nama Lengkap</th>
-                          <th scope="col" class="border-0">UserName</th>
-                          <th scope="col" class="border-0">Email</th>
-                          <th scope="col" class="border-0">Password</th>
-                          <th scope="col" class="border-0">NIM</th>
-                          <th scope="col" class="border-0">NoHp</th>
+                          <th scope="col" class="border-0">Id Position</th>
+                          <th scope="col" class="border-0">Nama</th>
+                          <th scope="col" class="border-0">Aksi</th>
+                        
                         </tr>
                       </thead>
                       <tbody>
                         <tr>
                           <td>1</td>
                           <td>Muhammad Rudy Darmawan</td>
-                          <td>Rudy</td>
-                          <td>Rudy@gmail.com</td>
-                          <td>12345678</td>
-                          <td>E31234567</td>
-                          <td>081234567890</td>
+                  
                         </tr>
                         <tr>
                           <td>2</td>
                           <td>Atikah Nuri Hazma</td>
-                          <td>Atikah</td>
-                          <td>Atikah@gmail.com</td>
-                          <td>12345678</td>
-                          <td>E31234567</td>
-                          <td>081234567890</td>
                         </tr>
                         <tr>
                           <td>3</td>
                           <td>Rahma Romadona Ayu Riswanti</td>
-                          <td>Rahma</td>
-                          <td>Rahma@gmail.com</td>
-                          <td>12345678</td>
-                          <td>E31234567</td>
-                          <td>081234567890</td>
                         </tr>
                         <tr>
                           <td>4</td>
                           <td>Mutia Budi Utami</td>
-                          <td>Mutia</td>
-                          <td>Mutia@gmail.com</td>
-                          <td>12345678</td>
-                          <td>E31234567</td>
-                          <td>081234567890</td>
                         </tr>
                         <tr>
                           <td>5</td>
                           <td>Nurlita Ayu Rakhmawati</td>
-                          <td>Ayu</td>
-                          <td>Ayu@gmail.com</td>
-                          <td>12345678</td>
-                          <td>E31234567</td>
-                          <td>081234567890</td>
                         </tr>
                         <tr>
                           <td>2</td>
                           <td>Marisa Setya Anggraini</td>
-                          <td>Marisa</td>
-                          <td>MArisa@gmail.com</td>
-                          <td>12345678</td>
-                          <td>E31234567</td>
-                          <td>081234567890</td>
                         </tr>
                       </tbody>
                     </table>

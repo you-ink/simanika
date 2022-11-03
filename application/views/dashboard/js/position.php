a4be18805e3af1e8e433bcade8000a0f2820581b
you(r)-ink <60732521+you-ink@users.noreply.github.com>
2022-11-03 18:48:50 +0700

    Update JS Position

---
+++ application/views/dashboard/js/position.php
@@ -0,0 +1,35 @@
+<script>
+	$(document).ready(function() {
+
+		function load_member(params = []) {
+			$("table.table").DataTable({
+				dom: "<'row'<'col-sm-12 mb-2'B>>lfrtip",
+			    lengthMenu: [[10, 25, 50, 100, -1], [10, 25, 50, 100, "All"]],
+				buttons: [
+			         { 
+			         	extend: 'excel', 
+			         	text: '<i class="fas fa-download"></i> Download Excel',
+			         	filename: 'Data Jabatan Himanika',
+			         	title: null,
+			         	className: 'btn btn-sm btn-success',
+			         	exportOptions: {
+		                    columns: [ 0, 1, 2, 3, 4 ]
+		                }
+			         },
+			         { 
+			         	extend: 'print', 
+			         	text: '<i class="fas fa-print"></i> Print',
+			         	title: 'Data Jabatan Himanika',
+			         	className: 'btn btn-sm btn-success',
+			         	exportOptions: {
+		                    columns: [ 0, 1, 2, 3, 4 ]
+		                }
+			         },
+			    ]
+			});
+		}
+
+		load_member();
+
+	})
+</script>
--- application/views/dashboard/position.php
+++ application/views/dashboard/position.php
@@ -2,20 +2,27 @@
 	<div class="page-header row no-gutters py-4">
 		<div class="col-12 col-sm-4 text-center text-sm-left mb-0">
 			<span class="text-uppercase page-subtitle">Position</span>
-			<h3 class="page-title">HIMANIKA Position</h3>
+			<h3 class="page-title">Data Jabatan</h3>
 		</div>
 	</div>
 	<div class="row">
               <div class="col">
                 <div class="card card-small mb-4">
                   <div class="card-header border-bottom">
-                    <h6 class="m-0">Data Position</h6>
+                    <div class="row">
+                      <div class="col-6">
+                        <h6 class="m-0">Data Jabatan</h6>
+                      </div>
+                      <div class="col-6 text-right">
+                        <button class="btn btn-sm btn-success"><i class="fas fa-plus"></i> Tambah Jabatan</button>
+                      </div>
+                    </div>
                   </div>
-                  <div class="card-body p-0 pb-3 text-center">
+                  <div class="card-body p-3">
                     <table class="table mb-0">
                       <thead class="bg-light">
                         <tr>
-                          <th scope="col" class="border-0">Id Position</th>
+                          <th scope="col" class="border-0">No</th>
                           <th scope="col" class="border-0">Nama</th>
                           <th scope="col" class="border-0">Aksi</th>
                         
@@ -25,28 +32,38 @@
                         <tr>
                           <td>1</td>
                           <td>Muhammad Rudy Darmawan</td>
-                          <td></td>
-                  
+                          <td>
+                            <a href="#">
+                              <button type="button" class="btn btn-sm btn-primary"><i class="fas fa-pen"></i></button>
+                            </a>
+                            <a href="#">
+                              <button type="button" class="btn btn-sm btn-danger"><i class="fas fa-trash"></i></button>
+                            </a>
+                          </td>
                         </tr>
                         <tr>
                           <td>2</td>
                           <td>Atikah Nuri Hazma</td>
+                          <td>
+                            <a href="#">
+                              <button type="button" class="btn btn-sm btn-primary"><i class="fas fa-pen"></i></button>
+                            </a>
+                            <a href="#">
+                              <button type="button" class="btn btn-sm btn-danger"><i class="fas fa-trash"></i></button>
+                            </a>
+                          </td>
                         </tr>
                         <tr>
                           <td>3</td>
                           <td>Rahma Romadona Ayu Riswanti</td>
-                        </tr>
-                        <tr>
-                          <td>4</td>
-                          <td>Mutia Budi Utami</td>
-                        </tr>
-                        <tr>
-                          <td>5</td>
-                          <td>Nurlita Ayu Rakhmawati</td>
-                        </tr>
-                        <tr>
-                          <td>2</td>
-                          <td>Marisa Setya Anggraini</td>
+                          <td>
+                            <a href="#">
+                              <button type="button" class="btn btn-sm btn-primary"><i class="fas fa-pen"></i></button>
+                            </a>
+                            <a href="#">
+                              <button type="button" class="btn btn-sm btn-danger"><i class="fas fa-trash"></i></button>
+                            </a>
+                          </td>
                         </tr>
                       </tbody>
                     </table>

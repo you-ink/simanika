c11f6be8a890fc75b8defb4509dcf79f89f97329
zakiaass <117088186+zakiaass@users.noreply.github.com>
2022-11-03 18:13:39 +0100

    update meeting

---
+++ application/views/dashboard/meeting.php
@@ -0,0 +1,115 @@
+<div class="main-content-container container-fluid px-4">
+	<div class="page-header row no-gutters py-4">
+		<div class="col-12 col-sm-4 text-center text-sm-left mb-0">
+			<span class="text-uppercase page-subtitle">Member</span>
+			<h3 class="page-title">HIMANIKA Meeting Agenda</h3>
+		</div>
+	</div>
+	<div class="row">
+              <div class="col">
+                <div class="card card-small mb-4">
+                  <div class="card-header border-bottom">
+                    <h6 class="m-0">Data Meeting Agenda</h6>
+                  </div>
+                  <div class="card-body p-0 pb-3 text-center">
+                    <table class="table mb-0">
+                      <thead class="bg-light">
+                        <tr>
+                          <th scope="col" class="border-0">No</th>
+                          <th scope="col" class="border-0">Tipe Agenda Rapat</th>
+                          <th scope="col" class="border-0">Nama Agenda Rapat</th>
+                          <th scope="col" class="border-0">Waktu</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>1</td>
+                          <td>Rapat Resmi</td>
+                          <td>Rapat 100 Hari Kerja</td>
+                          <td>3 September 2022</td>
+                          <td>
+                            <a href="#">
+                              <button type="button" class="btn btn-sm btn-primary"><i class="fas fa-pen"></i></button>
+                            </a>
+                            <a href="#">
+                              <button type="button" class="btn btn-sm btn-danger"><i class="fas fa-trash"></i></button>
+                            </a>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>2</td>
+                          <td>Rapat Resmi</td>
+                          <td>Rapat Rancangan Program Kerja</td>
+                          <td>10 September 2022</td>
+                          <td>
+                            <a href="#">
+                              <button type="button" class="btn btn-sm btn-primary"><i class="fas fa-pen"></i></button>
+                            </a>
+                            <a href="#">
+                              <button type="button" class="btn btn-sm btn-danger"><i class="fas fa-trash"></i></button>
+                            </a>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>3</td>
+                          <td>Rapat Program Kerja</td>
+                          <td>Konsep Acara Anjangsana</td>
+                          <td>10 Desember 2022</td>
+                          <td>
+                            <a href="#">
+                              <button type="button" class="btn btn-sm btn-primary"><i class="fas fa-pen"></i></button>
+                            </a>
+                            <a href="#">
+                              <button type="button" class="btn btn-sm btn-danger"><i class="fas fa-trash"></i></button>
+                            </a>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>4</td>
+                          <td>Rapat Program Kerja</td>
+                          <td>Konsep Acara Open Recruitment HIMANIKA</td>
+                          <td>20 Oktober 2022</td>
+                          <td>
+                            <a href="#">
+                              <button type="button" class="btn btn-sm btn-primary"><i class="fas fa-pen"></i></button>
+                            </a>
+                            <a href="#">
+                              <button type="button" class="btn btn-sm btn-danger"><i class="fas fa-trash"></i></button>
+                            </a>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>5</td>
+                          <td>Rapat Program Kerja</td>
+                          <td>Konsep Acara Management Informatics Competition</td>
+                          <td>2 November 2022</td>
+                          <td>
+                            <a href="#">
+                              <button type="button" class="btn btn-sm btn-primary"><i class="fas fa-pen"></i></button>
+                            </a>
+                            <a href="#">
+                              <button type="button" class="btn btn-sm btn-danger"><i class="fas fa-trash"></i></button>
+                            </a>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>6</td>
+                          <td>Rapat Resmi</td>
+                          <td>Rapat Akhir Tahun Periode</td>
+                          <td>10 Desember 2022</td>
+                          <td>
+                            <a href="#">
+                              <button type="button" class="btn btn-sm btn-primary"><i class="fas fa-pen"></i></button>
+                            </a>
+                            <a href="#">
+                              <button type="button" class="btn btn-sm btn-danger"><i class="fas fa-trash"></i></button>
+                            </a>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+</div>

6414888af4da9d82a7e3f3a38f35c818b08ac5cf
devyaprilliah <devyaprilliah6@gmail.com>
2022-11-28 13:48:46 +0700

    homepage.php

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -104,6 +104,18 @@ class Dashboard extends CI_Controller {
 		$data['script'] = get_views('dashboard/js/budget');
 		$this->load->view('app/dashboard/footer', $data);
 	}
+
+	public function homepage()
+	{
+		$data['title'] = 'Simanika - Homepage';
+
+		$this->load->view('app/dashboard/header', $data);
+		$this->load->view('app/dashboard/sidebar', $data);
+		$this->load->view('app/dashboard/navbar', $data);
+		$this->load->view('dashboard/homepage', $data);
+		$data['script'] = get_views('dashboard/js/homepage');
+		$this->load->view('app/dashboard/footer', $data);
+	}
 }
 
 /* End of file Dashboard.php */

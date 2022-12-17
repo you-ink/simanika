ba9f1e1e336954cdbcee91b2041c63ec351aba41
tutu <afandi38604@gmail.com>
2022-12-17 09:37:06 +0700

    img team

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -511,21 +511,19 @@
 
 				<div class="section-title">
 					<h2>Kepengurusan HIMANIKA Tahun Periode 2021/2022</h2>
-					<p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint
-						consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit
-						in iste officiis commodi quidem hic quas.</p>
+					<p> tim pengembang</p>
 				</div>
 
 				<div class="row">
 
 					<div class="col-lg-6">
 						<div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="100">
-							<div class="pic"><img src="<?php echo base_url("assets/template/arsha/") ?>assets/img/team/team-1.jpg"
+							<div class="pic"><img src="<?php echo base_url("assets/template/arsha/") ?>assets/img/a2/team-1.jpg"
 									class="img-fluid" alt=""></div>
 							<div class="member-info">
-								<h4>Walter White</h4>
-								<span>Chief Executive Officer</span>
-								<p>Explicabo voluptatem mollitia et repellat qui dolorum quasi</p>
+								<h4>Yongki Vanesta</h4>
+								<span>Full Stack Developerr</span>
+								<p>merancang sistem dari awal</p>
 								<div class="social">
 									<a href=""><i class="ri-twitter-fill"></i></a>
 									<a href=""><i class="ri-facebook-fill"></i></a>
@@ -538,12 +536,12 @@
 
 					<div class="col-lg-6 mt-4 mt-lg-0">
 						<div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="200">
-							<div class="pic"><img src="<?php echo base_url("assets/template/arsha/") ?>assets/img/team/team-2.jpg"
+							<div class="pic"><img src="<?php echo base_url("assets/template/arsha/") ?>assets/img/a2/team-2.jpg"
 									class="img-fluid" alt=""></div>
 							<div class="member-info">
-								<h4>Sarah Jhonson</h4>
-								<span>Product Manager</span>
-								<p>Aut maiores voluptates amet et quis praesentium qui senda para</p>
+								<h4>Ahmad Afandi</h4>
+								<span>Front End Developer</span>
+								<p>merancang komposisi tampilan</p>
 								<div class="social">
 									<a href=""><i class="ri-twitter-fill"></i></a>
 									<a href=""><i class="ri-facebook-fill"></i></a>
@@ -556,12 +554,12 @@
 
 					<div class="col-lg-6 mt-4">
 						<div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="300">
-							<div class="pic"><img src="<?php echo base_url("assets/template/arsha/") ?>assets/img/team/team-3.jpg"
+							<div class="pic"><img src="<?php echo base_url("assets/template/arsha/") ?>assets/img/a2/team-3.jpg"
 									class="img-fluid" alt=""></div>
 							<div class="member-info">
-								<h4>William Anderson</h4>
-								<span>CTO</span>
-								<p>Quisquam facilis cum velit laborum corrupti fuga rerum quia</p>
+								<h4>Ananda Zakia</h4>
+								<span>System Analyst</span>
+								<p>melakukan analisis</p>
 								<div class="social">
 									<a href=""><i class="ri-twitter-fill"></i></a>
 									<a href=""><i class="ri-facebook-fill"></i></a>
@@ -574,12 +572,12 @@
 
 					<div class="col-lg-6 mt-4">
 						<div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="400">
-							<div class="pic"><img src="<?php echo base_url("assets/template/arsha/") ?>assets/img/team/team-4.jpg"
+							<div class="pic"><img src="<?php echo base_url("assets/template/arsha/") ?>assets/img/a2/team-4.jpg"
 									class="img-fluid" alt=""></div>
 							<div class="member-info">
-								<h4>Amanda Jepson</h4>
-								<span>Accountant</span>
-								<p>Dolorum tempora officiis odit laborum officiis et et accusamus</p>
+								<h4>Devy Aprillia</h4>
+								<span>Designer</span>
+								<p>Merancang tampilan dari </p>
 								<div class="social">
 									<a href=""><i class="ri-twitter-fill"></i></a>
 									<a href=""><i class="ri-facebook-fill"></i></a>
@@ -760,6 +758,56 @@
 
 			</div>
 		</section><!-- End Activity Section -->
+
+		<!-- ======= Activity Section ======= -->
+		<section id="agenda" class="portfolio">
+			<div class="container" data-aos="fade-up">
+
+				<div class="section-title">
+					<h2>Agenda Kegiatan HIMANIKA Yang Masih Berjalan</h2>
+					<p>Informasi Himpunan Mahasiswa Manajemen Informatika Yang masih berjalan</p>
+				</div>
+
+				<ul id="portfolio-flters" class="d-flex justify-content-center" data-aos="fade-up" data-aos-delay="100">
+					<li data-filter="*" class="filter-active">All</li>
+					<li data-filter=".filter-humas_agenda">Humas</li>
+					<li data-filter=".filter-psdm_agenda">PSDM</li>
+					<li data-filter=".filter-kominfo_agenda">Kominfo</li>
+				</ul>
+
+				<div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
+
+					<div class="col-lg-4 col-md-6 portfolio-item filter-psdm_agenda">
+						<div class="portfolio-img"><img
+								src="<?php echo base_url("assets/template/arsha/") ?>assets/img/portfolio/psdm4.jpeg" class="img-fluid"
+								alt="">
+							<h4>Divisi PSDM</h4>
+							<p>Open Requitment Pengurus HIMANIKA 2022/2023</p>
+							<a href="<?php echo base_url("assets/template/arsha/") ?>assets/img/portfolio/psdm4.jpeg"
+								data-gallery="portfolioGallery" class="portfolio-lightbox preview-link"
+								title="Proker Divisi PSDM - Open Requitment Pengurus HIMANIKA 2022/2023"><i class="bx bx-plus"></i></a>
+							<a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+						</div>
+					</div>
+
+					<div class="col-lg-4 col-md-6 portfolio-item filter-kominfo_agenda">
+						<div class="portfolio-img"><img
+								src="<?php echo base_url("assets/template/arsha/") ?>assets/img/portfolio/kominfo1.jpeg"
+								class="img-fluid" alt="">
+							<h4>Divisi Kominfo</h4>
+							<p>Jasa Desain Grafis</p>
+							<a href="<?php echo base_url("assets/template/arsha/") ?>assets/img/portfolio/kominfo1.jpeg"
+								data-gallery="portfolioGallery" class="portfolio-lightbox preview-link"
+								title="Proker Divisi Kominfo - Jasa Desain Grafis"><i class="bx bx-plus"></i></a>
+							<a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+						</div>
+					</div>
+
+				</div>
+
+			</div>
+		</section><!-- End Activity Section -->
+
 
 		<!-- ======= Pricing Section ======= -->
 		<!-- <section id="pricing" class="pricing">

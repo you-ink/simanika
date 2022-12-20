215051931f44b30e329b1d70f00fce86dad6dcf9
zakiaass <117088186+zakiaass@users.noreply.github.com>
2022-12-20 12:56:52 +0700

    update visi misi

--- application/views/auth/register.php
+++ application/views/auth/register.php
@@ -41,8 +41,11 @@
 							<div class="col-lg-6">
 								<div class="p-5">
 									<div class="text-center">
-										<h1 class="h4 text-gray-900 mb-4">Welcome To Simanika</h1>
+										<h1 class="h4 text-gray-900 mb-4"><span>REGISTER</h1>
 									</div>
+									<div class="text-left">
+										<h6 class="h6 text-gray-900 mb-4">Thank You for joining us. Please register by 
+											completing the information below. </h6> <br>
 									<form>
 										<div class="form-row">
 											<div class="form-group col-md-6">

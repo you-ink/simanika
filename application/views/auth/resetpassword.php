223908571d6f38cc9aac99f14445644a5c254682
devyaprilliah <devyaprilliah6@gmail.com>
2022-12-21 12:49:39 +0700

    update resetpassword.php

--- application/views/auth/resetpassword.php
+++ application/views/auth/resetpassword.php
@@ -47,29 +47,21 @@
 										<h1 class="h5 mb-4" style="color:DodgerBlue;"><span><b>Welcome To SIMANIKA !</b></span></h1>
 									</div>
 									<hr color="white">
-									<h1 class="h5 mb-4 text-center" style="color:white;"><span style="Color:DodgerBlue;"><b>Login </b></span>Your Account</h1>  
+									<h1 class="h5 mb-4 text-center" style="color:white;"><span style="Color:DodgerBlue;"><b>Reset </b></span>Your Password</h1>  
 									<form class="user" action="login.php" method="POST">
 										<div class="form-group">
-											<input type="text" class="form-control form-control-user"
-												name="txt_email" placeholder="user name" required>
+											<input type="password" class="form-control form-control-user"
+                                            name="txt_pass" placeholder="New Password" required>
 										</div>
 										<div class="form-group">
 											<input type="password" class="form-control form-control-user"
-												name="txt_pass" placeholder="password" required>
+												name="txt_newpass" placeholder="Confirm password" required>
 										</div>
 										<div>
 											<input class="btn btn-primary btn-user btn-block" type="submit"
-												name="submit" value="Login">
+												name="submit" value="Confirm">
 										</div>
 									</form>
-									<div class="row">
-									    <div class="text-left col-6">
-										    <a class="small text-white mb-4" href="<?php echo base_url('register') ?>">Create an Account!</a>
-                                        </div>
-                                        <div class="text-right col-6">
-										<a class="small text-white mb-4" href="<?php echo base_url('forgotpassword') ?>">Forgot Your Password?</a>
-                                        </div>
-									</div>
 								</div>
 							</div>
 						</div>

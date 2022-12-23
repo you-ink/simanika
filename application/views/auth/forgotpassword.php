a048b7cc61b86d8e2f6b4c081c8680e971bac13f
zakiaass <117088186+zakiaass@users.noreply.github.com>
2022-12-23 10:57:34 +0700

    update about password

--- application/views/auth/forgotpassword.php
+++ application/views/auth/forgotpassword.php
@@ -14,14 +14,10 @@
 
 	<?= stylesheet([
       'plugin/fontawesome/css/all.min.css',
-      'plugin/bootstrap/css/bootstrap.min.css',
-      'template/shards-dashboard/styles/extras.1.1.0.min.css',
-      'plugin/DataTables/datatables.min.css',
+      'plugin/bootstrap/css/bootstrap.min.css'
     ]); ?>
 
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-	<link rel="stylesheet" id="main-stylesheet" data-version="1.1.0"
-		href="<?php echo base_url('assets/template/shards-dashboard/') ?>styles/shards-dashboards.1.1.0.min.css">
 
 </head>
 
@@ -38,27 +34,29 @@
 								<div class="col-md-2">
 									<img src="<?php echo base_url() ?>assets/img/logo.png" alt="simanika">
 								</div>
-								
+
 							</div>
 							<div class="col-lg-7" style="background-color:Black">
 								<div class="p-5">
-	                                <div class="text-center">
-	                                    <h1 class="h3 mb-0" style="color:white;"><span><b>Forgot Your Password ?</b></span></h1> 
-										</div>
+									<div class="text-center">
+										<h1 class="h3 mb-0" style="color:white;"><span><b>Forgot Your Password
+													?</b></span></h1>
 										<hr class="text-white bg-info">
-	                                    <h6 class="text-white mb-4">Enter email address that associated with your account </h6> <br>
+										<h6 class="text-white mb-4">Enter email address that associated with your
+											account </h6> <br>
 										<form class="user" action="login.php" method="POST">
 											<div class="form-group">
-												<input type="text" class="form-control form-control-user"
-													id="txt_email" placeholder="Enter your email" required>
+												<input type="text" class="form-control form-control-user" id="txt_email"
+													placeholder="Enter your email" required>
 											</div>
 											<div>
-												<button class="btn btn-primary btn-user btn-block btn-forgot-password" type="submit"
-													name="submit">Send Link</button> <br>
+												<button class="btn btn-primary btn-user btn-block btn-forgot-password"
+													type="submit" name="submit">Send Link</button> <br>
 											</div>
 											<div class="h6 text-right">
-												<a class="small text-white mb-4" href="<?php echo base_url('login') ?>">Back to Login</a>
-                                        	</div>
+												<a class="small text-white mb-4"
+													href="<?php echo base_url('login') ?>">Back to Login</a>
+											</div>
 										</form>
 									</div>
 								</div>
@@ -71,24 +69,25 @@
 	</div>
 
 	<script>
-    function get_api_url() {
-        return "<?php echo base_url('api/') ?>";
-    }
-  </script>
+		function get_api_url() {
+			return "<?php echo base_url('api/') ?>";
+		}
 
-  <?= script([
+	</script>
+
+	<?= script([
     'js/jquery-3.6.1.min.js',
     'plugin/bootstrap/js/bootstrap.bundle.min.js',
     'plugin/sweetalert2/sweetalert2.all.min.js',
     'js/main.js',
   ]); ?>
 
-  <script>
+	<script>
 		$(document).on('click', '.btn-forgot-password', function (e) {
 			e.preventDefault()
 			$(this).html("<i class='fas fa-spinner fa-pulse'></i>")
 			$(this).attr('disabled', true)
-			
+
 			data = {
 				email: $("input#txt_email").val()
 			}
@@ -97,26 +96,27 @@
 				pesan = req.message;
 				if (req.error == true) {
 					Swal.fire(
-			      'Gagal!',
-			      pesan,
-			      'error'
-			    ).then((result) => {
+						'Gagal!',
+						pesan,
+						'error'
+					).then((result) => {
 						$('.btn-forgot-password').html("Send Link")
 						$('.btn-forgot-password').attr('disabled', false)
-			    })
-				}else{
+					})
+				} else {
 					Swal.fire(
-		      	'Berhasil!',
-		      	pesan,
-		      	'success'
-		    	).then((result) => {
+						'Berhasil!',
+						pesan,
+						'success'
+					).then((result) => {
 						$('.btn-forgot-password').html("Send Link")
 						$('.btn-forgot-password').attr('disabled', false)
-			    })
+					})
 				}
 			})
 		})
-  </script>
+
+	</script>
 
 </body>
 
